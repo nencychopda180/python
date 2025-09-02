13a375cbfc51b98dbb3b39bc71934e42b43063af
nencychopda180 <nencychopda180@gmail.com>
2025-09-02 09:19:04 +0530

    a

--- pattern.py
+++ pattern.py
@@ -242,6 +242,39 @@
 # for i in range(5-1,0,-1):
 #     print(" "*(5-i) + "* " * i)
 
+'''
+* * * * * 
+ * * * * 
+  * * *
+   * *
+    *
+   * *
+  * * *
+ * * * *
+* * * * *
+'''
+
+# for i in range(5,0,-1):
+#     print(" "*(5-i)+"* "* i)
+# for i in range(2,5+1):
+#     print(" "*(5-i)+"* "* i)
+
+'''
+1 
+2 3 
+4 5 6
+7 8 9 10
+11 12 13 14 15
+'''
+
+# num=1
+# for i in range(1,5+1):
+#     for j in range(1,i+1):
+#         print(num,end=" ")
+#         num+=1
+#     print()
+
+
 
 
 
